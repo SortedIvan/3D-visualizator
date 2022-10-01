e8a60a3ab14e93a44b431a4b8d29dabc5b2fb8b3
Ovcharov,Ivan <450754@student.fontys.nl>
2022-10-02 01:05:48 +0200

    update: added selection sort & animations, working on abstraction

--- Assets/Scripts/main_controls.cs
+++ Assets/Scripts/main_controls.cs
@@ -20,8 +20,8 @@ public class main_controls : MonoBehaviour
 
 
     private GameObject temporary;
-     
-    
+
+
 
     void Start()
     {
@@ -38,26 +38,12 @@ public class main_controls : MonoBehaviour
 
 
         // Selection sort
-        selection_sort_test(unsorted_objects);
+        selection_sort(unsorted_objects);
     }
 
     void Update()
     {
-        
-    }
 
-    private IEnumerator wait_bubble_sort(List<GameObject> unsorted_objects) 
-    {
-        yield return new WaitForSeconds(5f);
-        StartCoroutine(
-            bubble_sort_script.sort_objects_bubble_sort(
-                unsorted_objects, red_material,white_material,green_material));
-
-    }
-
-    private void bubble_sort(List<GameObject> unsorted_objects)
-    {
-        StartCoroutine(wait_bubble_sort(unsorted_objects));
     }
 
     private void populate_list_obj()
@@ -72,39 +58,95 @@ public class main_controls : MonoBehaviour
 
     }
 
+    #region Bubble Sort
+    private IEnumerator wait_bubble_sort(List<GameObject> unsorted_objects)
+    {
+        yield return new WaitForSeconds(5f);
+        StartCoroutine(
+            bubble_sort_script.sort_objects_bubble_sort(
+                unsorted_objects, red_material, white_material, green_material));
+
+    }
+
+    private void bubble_sort(List<GameObject> unsorted_objects)
+    {
+        StartCoroutine(wait_bubble_sort(unsorted_objects));
+    }
+
+    #endregion
+
+    #region Selection Sort
+    private void selection_sort(List<GameObject> unsorted_objects)
+    {
+        StartCoroutine(selection_sort_test(unsorted_objects));
+    }
 
     private IEnumerator selection_sort_test(List<GameObject> unsorted_list)
     {
         GameObject min_value;
         GameObject temp;
-        for(int i = 0; i < unsorted_list.Count; i++) 
+        Transform parent1;
+        Transform parent2;
+        for (int i = 0; i < unsorted_list.Count; i++)
         {
             int minIndex = i;
-            unsorted_objects[minIndex].GetComponent<Renderer>().material = red_material;
             yield return new WaitForSeconds(1f);
             min_value = unsorted_list[i];
+            min_value.GetComponent<Renderer>().material = green_material; // First element red;
             for (int j = i + 1; j < unsorted_list.Count; j++)
             {
-                unsorted_objects[j].GetComponent<Renderer>().material = red_material;
+                yield return new WaitForSeconds(0.7f);
+                unsorted_list[j].GetComponent<Renderer>().material = red_material;
                 if (unsorted_list[j].GetComponent<number_obj>().object_value.
                     CompareTo(min_value.GetComponent<number_obj>().object_value) < 0)
                 {
                     // new smallest value
-                    unsorted_objects[j].GetComponent<Renderer>().material = green_material;
+                    yield return new WaitForSeconds(0.7f);
+                    min_value.GetComponent<Renderer>().material = white_material;
                     minIndex = j;
                     min_value = unsorted_list[j];
-
-
-
+                    min_value.GetComponent<Renderer>().material = green_material;
+                    continue;
                 }
+                yield return new WaitForSeconds(0.7f);
+                unsorted_list[j].GetComponent<Renderer>().material = white_material;
             }
 
             temp = unsorted_list[i];
             unsorted_list[i] = unsorted_list[minIndex];
             unsorted_list[minIndex] = temp;
+
+
+
+            parent1 = temp.transform.parent;
+            parent2 = unsorted_list[i].transform.parent;
+
+            unsorted_list[i].transform.position = parent1.position;
+            temp.transform.position = parent2.position;
+            
+
+            temp.gameObject.transform.SetParent(parent2);
+            unsorted_list[i].gameObject.transform.SetParent(parent1);
+
+            unsorted_list[i].gameObject.GetComponent<Renderer>().material = white_material;
+            unsorted_list[minIndex].gameObject.GetComponent<Renderer>().material = white_material;
+            yield return new WaitForSeconds(2f);
+
+
+
+        }
+
+
+        // Making all of the elements green after completion
+        for (int i = 0; i < unsorted_list.Count; i++)
+        {
+            yield return new WaitForSeconds(0.3f);
+            unsorted_list[i].GetComponent<Renderer>().material = green_material;
         }
 
     }
+
+    #endregion
 
 
 }
